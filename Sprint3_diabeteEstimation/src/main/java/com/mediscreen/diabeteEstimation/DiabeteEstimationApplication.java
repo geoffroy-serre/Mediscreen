d74d7db2c32045a2ca453e52c3893e2cede32f0f
GMSAnubis <geff1982@gmail.com>
2021-05-21 13:44:03 +0200

    sprint 3 initialization

---
+++ Sprint3_diabeteEstimation/src/main/java/com/mediscreen/diabeteEstimation/DiabeteEstimationApplication.java
@@ -0,0 +1,13 @@
+package com.mediscreen.diabeteEstimation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiabeteEstimationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiabeteEstimationApplication.class, args);
+	}
+
+}
---
+++ Sprint3_diabeteEstimation/src/test/java/com/mediscreen/diabeteEstimation/DiabeteEstimationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mediscreen.diabeteEstimation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiabeteEstimationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
